063235ca00bf678d29d836c115992fb5898c8521
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2022-09-17 16:36:15 +0200

    Update create_drop_alter.sql

--- create_drop_alter.sql
+++ create_drop_alter.sql
@@ -1,3 +1,13 @@
 DROP DATABASE IF EXISTS muhely; -- vizsgál arra gogy van ilyen adatbázis ha van törli ha nincs akkor nincs akkor nem történik semmi
 CREATE DATABASE muhely;
+USE muhely;
+
+CREATE TABLE szgk (
+	id INT, -- integer
+    tipus VARCHAR(20),  -- rövid szöveges info
+    modell VARCHAR(20),
+    rendszam VARCHAR(10),
+    ajtok_szama INT,
+    gyartas_eve DATE
+);
 
