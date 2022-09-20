1e0a3a8900cdf0121b9e515ab7ed1ed9495b457e
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2022-09-20 14:03:46 +0200

    Create fuggvenyek_feladat.sql

---
+++ fuggvenyek_feladat.sql
@@ -0,0 +1,44 @@
+/*A következő feladatokat
+- a Northwind tesztadatbázison oldd meg,
+- phpMyAdmin SQL beviteli mezőjében írd meg az utasításokat,
+- majd mentsd el ezeket egy SQL szkript formájában!
+
+1. Feladat
+A 'products' táblában határozd meg a 'standard_cost' mezőjének átlagértékét*/
+SELECT 
+/*
+a, az össezs rekord esetében,
+b, azon rekordok esetében, amelyeknél a 'list_price' 30-nál kisebb értékű,
+c, azon rekordok esetében, amelyeknél a 'category' mezőben 'Sauces' érték található!
+
+
+2. Feladat
+A 'products' táblában határozd meg a 'standard_cost' mezőjének összértékét
+a, az össezs rekord esetében,
+b, azon rekordok esetében, amelyeknél a 'list_price' értéke 20 és 50 közötti,
+c, azon rekordok esetében, amelyeknél a 'category' mezőben NEM 'Sauces' érték található!
+
+
+3. Feladat
+A 'employees' táblában határozd meg azon alkalmazottak számát
+a, akik Seattle-ben laknak,
+b, akiknek 'Sales Representative' munkakörben dolgoznak
+c, akiknek a keresztneve 'A' betűvel kezdődik!
+
+
+
+4. Feladat
+A 'products' táblában határozd meg a 'standard_cost' oszlop
+a, legkisebb értékét,
+b, legnagyobb értékét,
+c, legnagyobb értékét, azon rekordok esetében, ahol a 'list_price' legalább 30,
+d, legnagyobb értékét, azon rekordok esetében, ahol a 'product_code' tartalmazza a 'CO' sztringet!*/
+
+
+
+
+
+
+
+
+
