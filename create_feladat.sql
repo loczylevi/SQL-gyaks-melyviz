557b133c7f84c9d2c73e51e9aac6f42dd41aae09
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2022-09-20 12:53:11 +0200

    Create create_feladat.sql

---
+++ create_feladat.sql
@@ -0,0 +1,41 @@
+/*1. Feladat
+Az alábbi feladatot egy SQL szkript formájában valósítsd meg!
+Hozd létre el egy kutyamenhely adatbázisát és annak egy tábláját, amely a kutyák adatait tárolja az alábbi oszlopokban:
+- id: (azonosító), elsődleges kulcs, automatikus számozású
+- nev: nem lehet NULL
+- kor: értéke nem lehet több, mint 30
+- nem: alapértelemezett érték: 'kan'
+- megjegyzes: max. 500 karakter
+
+A táblában rögzíts minimum 2 rekordot!*/
+
+/*1. Feladat
+Az alábbi feladatot egy SQL szkript formájában valósítsd meg!
+Hozd létre el egy kutyamenhely adatbázisát és annak egy tábláját, amely a kutyák adatait tárolja az alábbi oszlopokban:
+- id: (azonosító), elsődleges kulcs, automatikus számozású
+- nev: nem lehet NULL
+- kor: értéke nem lehet több, mint 30
+- nem: alapértelemezett érték: 'kan'
+- megjegyzes: max. 500 karakter
+
+A táblában rögzíts minimum 2 rekordot!*/
+
+
+DROP DATABASE IF EXISTS kutyamenhely; 
+CREATE DATABASE kutyamenhely;
+USE kutyamenhely;
+
+CREATE TABLE kutya (
+    id INT PRIMARY KEY AUTO_INCREMENT, 
+    nev VARCHAR(20) NOT NULL,       
+    kor INT CHECK (kor < 30),          
+    nem VARCHAR(20) DEFAULT 'kan',       
+    megjegyzes VARCHAR(500)      -- itt már nem kell vessző fuuuuuuuuuuuuuuuuuuuu
+
+);
+
+
+INSERT INTO kutya (id,nev,kor,nem,megjegyzes)
+VALUES (11, "Buksi", 18,"szuka","Utálja a gumicsontot, minden fajta rovart megeszik különösen a darazsat de ugye egy gond van hogy a darázs megcsipi szerencsétlen torkát, száját és feldagad. Szájábol ne vegyél ki semmit ha jót akarsz magadnak. Chipezve, orvosilag elimert fajtiszta puli kutya. Kasztrált.");
+
+
