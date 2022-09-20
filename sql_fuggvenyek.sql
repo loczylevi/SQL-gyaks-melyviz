79c51b1db47927921ddaecb86797fb4cc0aa28b2
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2022-09-20 11:30:20 +0200

    Update sql_fuggvenyek.sql

--- sql_fuggvenyek.sql
+++ sql_fuggvenyek.sql
@@ -1,1 +1,4 @@
-
+USE northwind;
+-- DESCRIBE products;  -- products megjelenitése 
+SELECT MIN(list_price) FROM products;          -- a legkisebb értéket jeleniti meg a list_price-ból
+SELECT * FROM products ORDER BY list_price;      -- novekvo sorend a list_price-ra
