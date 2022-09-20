99ccc53de3c38acbeb033b21d6b6f440c4e921ef
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2022-09-20 11:12:08 +0200

    Update gyaks.sql

--- gyaks.sql
+++ gyaks.sql
@@ -3,13 +3,18 @@ CREATE DATABASE muhely;
 USE muhely;
 
 CREATE TABLE szgk (
-    id INT PRIMARY KEY AUTO_INCREMENT, -- integer , elsődleges kulcs 
-    tipus VARCHAR(20) DEFAULT 'Toyota',  -- rövid szöveges info alapártelmezettem toyota
-    modell VARCHAR(20) NOT NULL,          -- nme lehet üres
-    rendszam VARCHAR(10) UNIQUE, 
+    id INT PRIMARY KEY AUTO_INCREMENT, -- integer , elsődleges kulcs                     --PRIMARY KEY aq NOT NULL és a UNIQUE szerelem gyermeke
+    tipus VARCHAR(20) DEFAULT 'Toyota',  -- rövid szöveges info alapártelmezettem toyota                -- DEFAULT alapértelmezett érték      
+    modell VARCHAR(20) NOT NULL,          -- NOT NULL az adott oszlopban nem szerepelhet NULL érték
+    rendszam VARCHAR(10) UNIQUE,        -- UNIQUE nem szerepelhet két azonos érték egy oszlopban
     ajtok_szama INT CHECK (ajtok_szama < 6),           -- az ajtoknak kisebbnek kell lenie mint 6 
     gyartas_eve DATE
 );
+
+
+
+
+
 
 INSERT INTO szgk (id, tipus, modell, rendszam, ajtok_szama, gyartas_eve)         -- bővitjük az adatbázisunkat ezekkel az adatokkal
 VALUES (10, 'OPEL','Corsa','ABC123',4,'2002-10-04');       -- bővitjük az adatbázisunkat ezekkel az adatokkal
