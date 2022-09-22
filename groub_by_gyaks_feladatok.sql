00082c5586bfc9cfc05bcc9b4d5e8e25d3c3b409
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2022-09-22 16:51:19 +0200

    Create groub_by_gyaks_feladatok.sql

---
+++ groub_by_gyaks_feladatok.sql
@@ -0,0 +1,23 @@
+/*A következő feladatokat
+- a Northwind tesztadatbázison oldd meg,
+- phpMyAdmin SQL beviteli mezőjében írd meg az utasításokat,
+- majd mentsd el ezeket egy SQL szkript formájában!
+
+1. Feladat
+Az orders tábla ship_state_province mezőjének értékeit fésüld össze a customers tábla state_province mezőjének értékeivel úgy, hogy
+- minden érték csak egyszer szerepeljen,
+- az értékek ismétlődhetnek!*/
+
+
+
+/*2. Feladat
+A products táblában számold meg kategóriánként (category mező) a termékek számát!*/
+
+
+/*3. Feladat
+A orders táblában számold meg városonként (ship_city mező) a rendelések számát!*/
+
+
+/*4. Feladat
+A customers táblából kérd le az ügyfelek számát városonként, ha az eléri legalább a 3-at!
+*/
