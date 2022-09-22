0e61acd4f16d6ecbcb92af329a90af80fd89bd21
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2022-09-22 16:47:29 +0200

    Update groub_by.sql

--- groub_by.sql
+++ groub_by.sql
@@ -1,3 +1,4 @@
+-- https://sql.sulipy.hu/sql2/group_by?tab=peldak
 /* UNION 
 mindegyik táblából azonos számú oszlopot kell megadnunk,
 azonos sorrendben,
@@ -7,3 +8,38 @@ SELECT City FROM Customers
 UNION 
 SELECT City FROM Suppliers 
 ORDER BY City
+
+
+
+SELECT City, Country FROM Customers
+WHERE Country='Germany'
+UNION ALL
+SELECT City, Country FROM Suppliers
+WHERE Country='Germany'
+ORDER BY City;
+
+
+SELECT 'Customer' AS Type, ContactName, City, Country
+FROM Customers
+UNION
+SELECT 'Supplier', ContactName, City, Country
+FROM Suppliers;
+
+
+-- GROUP BY
+
+SELECT COUNT(CustomerID), Country
+FROM Customers
+GROUP BY Country
+ORDER BY COUNT(CustomerID) DESC;
+
+
+-- HAVING
+-- (szükséges, mivel a WHERE záradék esetén nem lehet összegző függvényeket használni)
+
+SELECT COUNT(CustomerID), Country
+FROM Customers
+GROUP BY Country
+HAVING COUNT(CustomerID) > 5
+ORDER BY COUNT(CustomerID) DESC;
+  
