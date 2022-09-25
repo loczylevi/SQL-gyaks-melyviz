90f3b0f53c1dfa5b275e664f301b87b978480298
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2022-09-25 17:39:15 +0200

    Create tablak_kapcsolodasa_feladat.sql

---
+++ tablak_kapcsolodasa_feladat.sql
@@ -0,0 +1,2 @@
+/*1. Feladat
+A CREATE, DROP, ALTER című lecke kódjában található 'muhely' adatbazist egeszitsd ki egy az autótulajdonosok adatait tároló táblával! A két táblát kapcsold össze, úgy hogy a most létrehozott tábla legyen a gyermektábla! A táblákat töltsd fel néhány adattal!*/
