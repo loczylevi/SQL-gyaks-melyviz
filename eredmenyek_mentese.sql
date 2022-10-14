774f0ac9077b4a5400a1dfef743c95bb8878e6bf
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2022-10-14 13:20:23 +0200

    Update eredmenyek_mentese.sql

--- eredmenyek_mentese.sql
+++ eredmenyek_mentese.sql
@@ -1,11 +1,24 @@
 /*1. Feladat
 A Northwind tesztadatbázis customers táblájából kérd le az ügyfelek számát városonként,
 ha az eléri legalább a 3-at, és az eredményt mentsd el az x1_customers nevű táblába!*/
+USE northwind;
+
+DROP TABLE IF EXISTS x1_customers;
+
+CREATE TABLE x1_customers
+    SELECT COUNT(city) AS ugyfelek_szama, city
+	FROM customers
+	GROUP BY city
+    HAVING COUNT(city) > 3;
+    
+/*2. Feladat
+A Northwind tesztadatbázis products táblájában számold meg kategóriánként (category mező) a termékek számát,
+és az eredményt mentsd el az x2_products nevű táblába!*/
 
 USE northwind;
 
-CREATE TABLE x12_customers
-    SELECT COUNT(id) AS ugyfelek, city
-    FROM customers
-    HAVING COUNT(id) > 3;
-    
+CREATE TABLE x2_products
+    SELECT COUNT(category) AS számláló, category
+	FROM products
+	GROUP BY category;
+   
